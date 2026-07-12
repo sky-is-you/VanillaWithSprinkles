379e236719a781275f96d669d7ee9d525a5cc946
sky-is-you <299973216+sky-is-you@users.noreply.github.com>
2026-07-12 01:42:14 -0400

    remove stray folder

--- Source/Meta/SkysOverworldCoreMeta.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Celeste.Mod.SkysOverworldCore.Meta;
-
-public class SkysOverworldCoreMeta
-{
-    public float OverlayOpacity { get; set; } = 0.45f;
-    public bool ShowVignette { get; set; } = true;
-}
