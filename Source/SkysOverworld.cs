94d01d254866a6cf8928cdb4491300f7113bee0a
sky-is-you <299973216+sky-is-you@users.noreply.github.com>
2026-07-12 01:54:25 -0400

    Add test scene (our overworld)

---
+++ Source/SkysOverworld.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Monocle;
+
+namespace Celeste.Mod.SkysOverworld;
+
+public class SkysOverworld : Scene
+{
+    public HiresSnow Snow;
+    public SkysOverworld(OverworldLoader loader)
+    {
+        Add(Snow = loader.Snow);
+    }
+
+    public override void Render()
+    {
+        base.Render();
+        Draw.SpriteBatch.Begin(SpriteSortMode.Immediate,BlendState.NonPremultiplied);
+        ActiveFont.Draw("Test scene\nHello world\nSky's Overworld v0.2.0 Overhaul", Vector2.Zero, Color.White);
+    }
+}
